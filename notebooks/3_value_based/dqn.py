17af603c2dbede826a821ec8b33d72ecce226488
kimsooyoung <tge1375@naver.com>
2024-10-26 19:50:02 -0700

    Daniel - dqn  cmt

--- notebooks/3_value_based/dqn.py
+++ notebooks/3_value_based/dqn.py
@@ -12,6 +12,13 @@ gamma         = 0.98
 buffer_limit  = 50000
 batch_size    = 32
 
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+print(f"Using device: {device}")
+
+
 class ReplayBuffer():
     def __init__(self):
         self.buffer = collections.deque(maxlen=buffer_limit)
@@ -31,9 +38,12 @@ class ReplayBuffer():
             s_prime_list.append(s_prime)
             done_list.append([dm])
 
-        return torch.tensor(s_list, dtype=torch.float), torch.tensor(a_list), \
-               torch.tensor(r_list), torch.tensor(s_prime_list, dtype=torch.float), \
-               torch.tensor(done_list)
+        return torch.tensor(s_list, dtype=torch.float).to(device), torch.tensor(a_list).to(device), \
+               torch.tensor(r_list).to(device), torch.tensor(s_prime_list, dtype=torch.float).to(device), \
+               torch.tensor(done_list).to(device)
+    
+    def size(self):
+        return len(self.buffer)
 
 
 class Qnet(nn.Module):
@@ -48,9 +58,11 @@ class Qnet(nn.Module):
         )
 
     def forward(self, x):
+        x = x.to(device)
         return self.layer(x)
 
     def sample_action(self, obs, epsilon):
+        obs = obs.to(device)
         pred = self.forward(obs)
         coin = random.random()
 
@@ -59,22 +71,65 @@ class Qnet(nn.Module):
         else:
             return pred.argmax().item()
 
-env = gym.make("CartPole-v1", render_mode = "human")
+
+def train(Qnet, Qnet_target, buffer, optim):
+    for i in range(10):
+        s,a,r,s_prime,dm = buffer.sample(batch_size)
+
+        q_out = Qnet(s)
+        q_pred = q_out.gather(1, a)
+
+        q_target_out = Qnet_target(s_prime)
+        q_target_pred = r + gamma * q_target_out.max(1)[0].unsqueeze(1) * dm
+
+        loss = F.smooth_l1_loss(q_pred, q_target_pred)
+
+        optim.zero_grad()
+        loss.backward()
+        optim.step()
+
+
+# env = gym.make("CartPole-v1", render_mode = "human")
+env = gym.make("CartPole-v1")
 print(f"env.action_space: {env.action_space}")
 print(f"env.observation_space: {env.observation_space}")
 
-s, _ = env.reset()
+q = Qnet().to(device)
+q_target = Qnet().to(device)
+q_target.load_state_dict(q.state_dict())
 
-replay_buffer = ReplayBuffer()
-q_net = Qnet()
+memory = ReplayBuffer()
 
-# while True:
-#     a = env.action_space.sample()
-#     s, r, done, truncated, info = env.step(a)
+optimizer = optim.Adam(q.parameters(), lr=learning_rate)
+target_update_interval = 20
+print_interval = 100
+epoch = 5000
 
-#     if done or truncated:
-#         break
+for i in range(epoch):
+    epsilon = 1.0 / (i + 1)
+    s, _ = env.reset()
+    score = 0.0
 
-#     env.render()
+    while True:
+        a = q.sample_action(torch.from_numpy(s).float(), epsilon)
+        s_prime, r, done, truncated, _ = env.step(a)
+        dm = 0.0 if done else 1.0
+        memory.put((s, a, r/100.0, s_prime, dm))
+
+        score += r
+        s = s_prime
+        if done or truncated:
+            break
+
+        if memory.size() > 2000:
+            train(q, q_target, memory, optimizer)
+        
+        if i % target_update_interval == 0 :
+            q_target.load_state_dict(q.state_dict())
+
+        if i % print_interval == 0 and i != 0:
+            print("n_episode :{}, score : {:.1f}, n_buffer : {}, eps : {:.1f}%".format(
+                                                            i, score, memory.size(), epsilon*100))
+
 
 env.close()
